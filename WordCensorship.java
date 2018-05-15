957008be902afa86b1974d4b6ff0d3d4ca43a016
richardmin11 <39314479+richardmin11@users.noreply.github.com>
2018-05-15 16:57:05 -0400

    WordCensorship.java

---
+++ WordCensorship.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class WordCensorship {
+	public static void main(String [] args) {
+		Scanner scanner = new Scanner(System.in);		
+		String adjective;
+		boolean isInvalidWord;
+		
+		do {
+			System.out.println("Enter an adjective which describes the great Richard:");
+			adjective = scanner.nextLine();
+			String adjectiveLowerCase = adjective.toLowerCase();
+			isInvalidWord = (adjectiveLowerCase.contains("lame") ||
+							         adjectiveLowerCase.contains("not funny"));
+			if(isInvalidWord) {
+				System.out.printf("This word is not allowed. Try again. %n %n");
+			}											 		
+		} while (isInvalidWord);
+		
+		System.out.printf("Richard is %s!", adjective);
+	}
+}
